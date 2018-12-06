f67b02cf7ee8b2ad079e7014eff4b6cada4e9537
Andie <zchangan@163.com>
2018-12-06 13:29:47 +0800

    :tada: Initial commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+import {SyncStorage} from 'kevast';
+
+class MemoryStorage extends SyncStorage {
+  private map: Map<string, string>;
+  public constructor() {
+    super();
+    this.map = new Map();
+  }
+  public clear(): void {
+    this.map.clear();
+  }
+  public has(key: string): boolean {
+    return this.map.has(key);
+  }
+  public delete(key: string): void {
+    this.map.delete(key);
+  }
+  public entries(): IterableIterator<[string, string]> {
+    return this.map.entries();
+  }
+  public get(key: string): string | undefined {
+    return this.map.get(key);
+  }
+  public keys(): IterableIterator<string> {
+    return this.map.keys();
+  }
+  public set(key: string, value: string): void {
+    this.map.set(key, value);
+  }
+  public size(): number {
+    return this.map.size;
+  }
+  public values(): IterableIterator<string> {
+    return this.map.values();
+  }
+}
